07b2411e37918aa4798c2ecf6b810ee0a35af3d5
yaphetswtl519 <1101544275@qq.com>
2018-06-06 16:23:17 +0800

    delete student & remove mobile translate

--- public/js/pages/pageInit.js
+++ public/js/pages/pageInit.js
@@ -1,7 +1,14 @@
 import move from 'move-js';
 
 export default function () {
-    if (window.scrollY) {
+    let userAgent = navigator.userAgent;
+    if (window.scrollY 
+        || userAgent.indexOf('Android') > -1 
+        || userAgent.indexOf('iPhone') > -1 
+        || userAgent.indexOf('iPad') > -1 
+        || userAgent.indexOf('iPod') > -1 
+        || userAgent.indexOf('Symbian') > -1
+    ) {
         $('.duyi-dom-translate').removeClass('duyi-dom-translate');
     }
     $('.duyi-dom-translate').each((index, item) => {
